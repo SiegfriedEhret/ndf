ea923ebdea64825ace58b617a076157e656be3f0
Siegfried Ehret <siegfried@ehret.me>
2017-02-21 18:59:18 +0100

    :wrench: update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gox -output="bin/{{.OS}}/{{.Arch}}/{{.Dir}}" -osarch="linux/amd64 darwin/amd64 windows/amd64"
+gox -output="bin/{{.OS}}/{{.Arch}}/{{.Dir}}" -osarch="linux/amd64"
